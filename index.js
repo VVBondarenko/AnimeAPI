e3923010e02f2dc58ca62b08922e03cb1f6de0bd
IGRohan <volumezero2508@gmail.com>
2022-07-25 16:56:24 +0530

    removing unused code

--- index.js
+++ index.js
@@ -89,16 +89,6 @@ app.get('/gogoanime/watch/:episodeId', async (req, res) => {
     res.json(data).status(200)
 });
 
-// app.get('/testing', async (req, res) => {
-//     // const resp = await axios.get('https://animixplay.to/rsssub.xml');
-//     // const jsonXml = xmltoJSON(resp.data);
-//     const testString = "aHR0cHM6Ly9tYW5pZmVzdC5wcm9kLmJvbHRkbnMubmV0L21hbmlmZXN0L3YxL2hscy92NC9jbGVhci82MzEyNzQ5ODI4MDAxL2Y0OGU3YjczLWE5MmItNDhmOC05YzliLWY4MmIwOGEzOTI0Ni82cy9tYXN0ZXIubTN1OD9mYXN0bHlfdG9rZW49TmpKa1lqQTROemxmTWpnMU1HSmtaakUyWXpVMFpqbG1aakZtTUdSa1pqQmlaVGhrWm1abU1UUmpZak0zWXpobE5ESTFPRGM0WW1FNU4ySTJaREV6WXpnd05tRTRNalprT0ElM0QlM0Q="
-//     const yo = "https://manifest.prod.boltdns.net/manifest/v1/hls/v4/clear/6312749828001/f48e7b73-a92b-48f8-9c9b-f82b08a39246/6s/master.m3u8?fastly_token=NjJkYjA4NzlfMjg1MGJkZjE2YzU0ZjlmZjFmMGRkZjBiZThkZmZmMTRjYjM3YzhlNDI1ODc4YmE5N2I2ZDEzYzgwNmE4MjZkOA%3D%3D"
-//     const decodedString = decodeString(testString)
-//     const encodedString = encodeString(yo)
-//     console.log(encodedString)
-// })
-
 //Start the web-server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
